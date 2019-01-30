47cb93fc8face818aceb5f7a002a2c975ebab1f7
tdefosse <thomas.defosse@insa-rouen.fr>
2019-01-30 10:12:06 +0100

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,14 @@
+{
+  // Drive 100 ms forward
+  bitbot.motor(BBMotor.All, 1023);
+  basic.pause(100);
+
+  // Drive 100 ms reverse
+  bitbot.motor(BBMotor.All, -1023);
+  basic.pause(100);
+
+  // Drive 100 ms forward on left and reverse on right
+  bitbot.motor(BBMotor.Left, 1023);
+  bitbot.motor(BBMotor.Right, -1023);
+  basic.pause(100);
+  }
