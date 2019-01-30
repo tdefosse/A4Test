e66750f4b4cb62460d40545b2efeaa1b750d205d
tdefosse <thomas.defosse@insa-rouen.fr>
2019-01-30 10:07:23 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,255 @@
+/**
+  * Enumeration of motors.
+  */
+enum BBMotor
+{
+    //% block="left"
+    Left,
+    //% block="right"
+    Right,
+    //% block="both"
+    Both
+}
+
+/**
+  * Enumeration of directions.
+  */
+enum BBRobotDirection
+{
+    //% block="left"
+    Left,
+    //% block="right"
+    Right
+}
+
+/**
+  * Values for buzzer. On or Off
+  */
+enum BBBuzz
+{
+    //% block="on"
+    On,
+    //% block="off"
+    Off
+}
+
+/**
+  * Enumeration of line sensors.
+  */
+enum BBLineSensor
+{
+    //% block="left"
+    Left,
+    //% block="right"
+    Right
+}
+
+/**
+  * Enumeration of light sensors.
+  */
+enum BBLightSensor
+{
+    //% block="left"
+    Left,
+    //% block="right"
+    Right
+}
+
+/**
+ * Ping unit for sensor.
+ */
+enum BBPingUnit
+{
+    //% block="cm"
+    Centimeters,
+    //% block="inches"
+    Inches,
+    //% block="μs"
+    MicroSeconds
+}
+
+/**
+  * Update mode for LEDs
+  * setting to Manual requires show LED changes blocks
+  * setting to Auto will update the LEDs everytime they change
+  */
+enum BBMode
+{
+    Manual,
+    Auto
+}
+
+/**
+  * Pre-Defined LED colours
+  */
+enum BBColors
+{
+    //% block=red
+    Red = 0xff0000,
+    //% block=orange
+    Orange = 0xffa500,
+    //% block=yellow
+    Yellow = 0xffff00,
+    //% block=green
+    Green = 0x00ff00,
+    //% block=blue
+    Blue = 0x0000ff,
+    //% block=indigo
+    Indigo = 0x4b0082,
+    //% block=violet
+    Violet = 0x8a2be2,
+    //% block=purple
+    Purple = 0xff00ff,
+    //% block=white
+    White = 0xffffff,
+    //% block=black
+    Black = 0x000000
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=50 color=#e7660b icon="\uf1b9"
+namespace bitbot
+{
+    let neoStrip: neopixel.Strip;
+    let _updateMode = BBMode.Auto;
+    let leftSpeed = 0;
+    let rightSpeed = 0;
+
+// Motor Blocks
+
+    // slow PWM frequency for slower speeds to improve torque
+    function setPWM(): void
+    {
+        if ((leftSpeed < 200) || (rightSpeed < 200))
+            pins.analogSetPeriod(AnalogPin.P0, 60000);
+        else if ((leftSpeed < 300) || (rightSpeed < 300))
+            pins.analogSetPeriod(AnalogPin.P0, 40000);
+        else
+            pins.analogSetPeriod(AnalogPin.P0, 30000);
+    }
+
+    /**
+      * Drive robot forward (or backward) at speed.
+      * @param speed speed of motor between -1023 and 1023. eg: 600
+      */
+    //% blockId="bitbot_motor_forward" block="drive at speed %speed"
+    //% speed.min=-1023 speed.max=1023
+    //% weight=100
+    //% subcategory=Motors
+    export function drive(speed: number): void
+    {
+        motor(BBMotor.Both, speed);
+    }
+
+    /**
+      * Drive robot forward (or backward) at speed for milliseconds.
+      * @param speed speed of motor between -1023 and 1023. eg: 600
+      * @param milliseconds duration in milliseconds to drive forward for, then stop. eg: 400
+      */
+    //% blockId="bitbot_motor_forward_milliseconds" block="drive at speed %speed| for %milliseconds|(ms)"
+    //% speed.min=-1023 speed.max=1023
+    //% weight=95
+    //% subcategory=Motors
+    export function driveMilliseconds(speed: number, milliseconds: number): void
+    {
+        drive(speed);
+        basic.pause(milliseconds);
+        drive(0);
+    }
+
+    /**
+      * Turn robot in direction at speed.
+      * @param direction direction to turn.
+      * @param speed speed of motor between 0 and 1023. eg: 600
+      */
+    //% blockId="bitbot_turn" block="spin %direction|at speed %speed"
+    //% speed.min=0 speed.max=1023
+    //% weight=90
+    //% subcategory=Motors
+    export function driveTurn(direction: BBRobotDirection, speed: number): void
+    {
+        if (speed < 0)
+            speed = 0;
+        if (direction == BBRobotDirection.Left)
+        {
+            motor(BBMotor.Left, -speed);
+            motor(BBMotor.Right, speed);
+        }
+        else if (direction == BBRobotDirection.Right)
+        {
+            motor(BBMotor.Left, speed);
+            motor(BBMotor.Right, -speed);
+        }
+    }
+
+    /**
+      * Spin robot in direction at speed for milliseconds.
+      * @param direction direction to turn.
+      * @param speed speed of motor between 0 and 1023. eg: 600
+      * @param milliseconds duration in milliseconds to turn for, then stop. eg: 400
+      */
+    //% blockId="bitbot_turn_milliseconds" block="spin %direction|at speed %speed| for %milliseconds|(ms)"
+    //% speed.min=0 speed.max=1023
+    //% weight=85
+    //% subcategory=Motors
+    export function driveTurnMilliseconds(direction: BBRobotDirection, speed: number, milliseconds: number): void
+    {
+        driveTurn(direction, speed)
+        basic.pause(milliseconds)
+        motor(BBMotor.Both, 0)
+    }
+
+    /**
+      * Drive motor(s) forward or reverse.
+      * @param motor motor to drive.
+      * @param speed speed of motor (-1023 to 1023). eg: 600
+      */
+    //% blockId="bitbot_motor" block="drive %motor|motor(s) at speed %speed"
+    //% weight=80
+    //% subcategory=Motors
+    export function motor(motor: BBMotor, speed: number): void
+    {
+        let forward = (speed >= 0);
+        let absSpeed = Math.abs(speed);
+        if ((motor == BBMotor.Left) || (motor == BBMotor.Both))
+            leftSpeed = absSpeed;
+        if ((motor == BBMotor.Right) || (motor == BBMotor.Both))
+            rightSpeed = absSpeed;
+        setPWM();
+
+        if (speed > 1023)
+            speed = 1023;
+        else if (speed < -1023)
+            speed = -1023;
+
+        let realSpeed = speed;
+        if (!forward)
+        {
+            if (realSpeed >= -200)
+                realSpeed = Math.idiv(realSpeed * 19, 6);
+            else if (realSpeed >= -400)
+                realSpeed = realSpeed * 2;
+            else if (realSpeed >= -600)
+                realSpeed = Math.idiv(realSpeed * 3, 2);
+            else if (realSpeed >= -800)
+                realSpeed = Math.idiv(realSpeed * 5, 4);
+            realSpeed = 1023 + realSpeed; // realSpeed is negative!
+        }
+
+        if ((motor == BBMotor.Left) || (motor == BBMotor.Both))
+        {
+            pins.analogWritePin(AnalogPin.P0, realSpeed);
+            pins.digitalWritePin(DigitalPin.P8, forward ? 0 : 1);
+        }
+
+        if ((motor == BBMotor.Right) || (motor == BBMotor.Both))
+        {
+            pins.analogWritePin(AnalogPin.P1, realSpeed);
+            pins.digitalWritePin(DigitalPin.P12, forward ? 0 : 1);
+        }
+    }
+
+
+}
